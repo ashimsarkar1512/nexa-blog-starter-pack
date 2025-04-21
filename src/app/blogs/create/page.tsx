7c0796ff2c3700878247b7306b270ef9f239cf55
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-04-21 23:49:39 +0600

    create the form and set the from action

---
+++ src/app/blogs/create/page.tsx
@@ -0,0 +1,12 @@
+import BlogForm from "@/components/ui/BlogForm";
+
+
+const CreateBlogPage = () => {
+            return (
+                        <div>
+                      <BlogForm></BlogForm>     
+                        </div>
+            );
+};
+
+export default CreateBlogPage;
